1f647f9b69346366f9a8e42d2924ee1db53876e1
ri94909-00 <85773984+ri94909-00@users.noreply.github.com>
2022-01-13 15:36:42 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,39 @@
+class launcher{
+    constructor(body,anchor){
+        var options = {
+            bodyA: body,
+            pointB: anchor,
+            stiffness: 0.004,
+            length: 1
+        }
+       
+        this.pointB = anchor
+        this.bodyA = body;
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+    attach(body){
+
+    this.launcher.bodyA = body;
+
+    }
+    
+    
+    fly(){
+        this.launcher.bodyA = null;
+    }
+    display(){
+
+    if(this.launcher.bodyA){
+      var pointA =this.bodyA.position;
+      var pointB = this.pointB;
+      strokeWeight(2.5);
+      line(pointA.x,pointA.y,pointB.x,pointB.y);
+
+
+    }
+    
+
+    }
+
+}
